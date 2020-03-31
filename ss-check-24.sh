b813e779ea292889abb7c07a15bd723f9573da04
Roy Thia <roy.thia@oss.com.ph>
2020-04-01 03:05:44 +0800

    update

--- ss-check-24.sh
+++ ss-check-24.sh
@@ -2,7 +2,7 @@
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 is_error=$(tail -n 1 /var/log/shadowsocks.log | grep -c "\[Errno 24\] Too many open files")
 
-if [$is_error = 1]; then
+if ["${is_error}" = "1"]; then
 	/etc/init.d/shadowsocks-python stop && /etc/init.d/shadowsocks-python start
 	echo "[$current_time] An error was found and Shadowsocks service has been restarted." >> /var/log/ss-check.log
 else
